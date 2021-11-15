3fa6371bf289548a214a8f556638cb38af39bbd9
rayane hachem <rayanehchm@gmail.com>
2021-11-15 17:38:56 +0100

    seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
-  has_many :items
+  has_many :bookings, dependent: :destroy
+  has_many :items, dependent: :destroy
 
   validates :name, :phone_number, presence: true
 end
