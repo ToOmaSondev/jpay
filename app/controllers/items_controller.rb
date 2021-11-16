89180b0ff8fa02178e544ff51449d4b7e65659f8
Max Ellis <uraniumax@gmail.com>
2021-11-16 19:32:18 +0100

    added photo upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,10 +21,8 @@ class ItemsController < ApplicationController
     end
   end
 
-private
-
   def item_params
-    params.require(:item).permit(:name, :price)
+    params.require(:item).permit(:name, :price, :photo)
   end
 
 end
