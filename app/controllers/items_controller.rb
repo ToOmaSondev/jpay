02c31fe0ac40c89888a82f5b0762b09739c8835b
rayane hachem <rayanehchm@gmail.com>
2021-11-16 15:36:11 +0100

    fin du controlleur items sans la methode destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,24 @@ class ItemsController < ApplicationController
   @item = Item.find(params[:id])
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save!
+    redirect_to item_path(@item)
+    else
+    render :new
+    end
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :price)
+  end
+
 end
