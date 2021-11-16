9292a64137695b5d0ec8cac2c96f7d604cba6ad2
Thomas Petrelli <petrelli.thomas@orange.fr>
2021-11-16 18:09:38 +0100

    Create/new okk

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,21 @@ class BookingsController < ApplicationController
   end
 
   def create
-
+    @item = Item.find(params[:item_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.item = @item
+    if @booking.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:begin_date, :end_date)
+  end
+
 end
