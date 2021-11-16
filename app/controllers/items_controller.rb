c829dd253120a01a7028de151ca064dff4432e54
rayane hachem <rayanehchm@gmail.com>
2021-11-16 15:41:31 +0100

    suppression du point dexclamation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.user = current_user
-    if @item.save!
+    if @item.save
     redirect_to item_path(@item)
     else
     render :new
